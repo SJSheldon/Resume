1206046c4bd1cdf1150d0d69514c3947e68e9244
Nekidsteve <Nekidsteve@Mjolnir.local>
2015-05-01 14:48:27 -0400

    initial

---
+++ js/week3.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+    new WOW().init();
+
+    $("#owl-example").owlCarousel();
+
+    $('.timer').countTo();
+
+    $(".rotate").textrotator({
+        animation: "dissolve", // You can pick the way it animates when rotating through words. Options are dissolve (default), fade, flip, flipUp, flipCube, flipCubeUp and spin.
+        separator: ",", // If you don't want commas to be the separator, you can define a new separator (|, &, * etc.) by yourself using this field.
+        speed: 2000 // How many milliseconds until the next word show.
+    });
+    smoothScroll.init();
+
+
+
+
+});
